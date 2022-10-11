9c628789ecb8d85e1cbadadc69c4b34ed85de3fe
Darya <daryabobur@gmail.com>
2022-10-11 21:58:23 +0300

    Update ContactsList.jsx

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,20 +1,26 @@
 import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid';
 import { Contacts } from './ContactsListStyled';
+import { useSelector, useDispatch } from 'react-redux';
+import {removeContact} from '../../redux/slice'
 
-const ContactsList = ({ contacts, removeContact }) => {
+const ContactsList = () => {
+   const dispatch = useDispatch();
+   const contact = useSelector(state => state.contacts);
+   console.log(contact.id)
+   // const handleDelete = () => dispatch(removeContact())
+   // console.log(handleDelete())
    return (  
       <Contacts>
-         {contacts.map(({ name, number, id }) => 
-            <li key={nanoid()}>
+         {contact.map(({ name, number, id }) => 
+            <li key={id}>
                <p>- {name}: {number}</p>
-               <button type='button' onClick={() => removeContact(id)}>Delete</button>
+               <button type='button' onClick={()=> dispatch(removeContact(id))}>Delete</button>
             </li>)
          }
       </Contacts>
    )
 }
-
 
 ContactsList.propTypes = {
    removeContact: PropTypes.func,
