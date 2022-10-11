fd0c82042d49e3b7896a8dfb77d39f1e14d3d8f8
Darya <daryabobur@gmail.com>
2022-10-11 21:58:11 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { nanoid } from 'nanoid';
+// import { nanoid } from 'nanoid';
 import ContactForm from '../ContactsForm/ContactsForm';
 import ContactsList from '../ContactsList/ContactsList';
 import Filter from '../Filter/Filter';
@@ -7,78 +7,99 @@ import { ContainerApp, Title, Subtitle } from './AppStyled';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import { configureStore } from '@reduxjs/toolkit'
+import { addContact, contactsSlice } from 'redux/slice';
+import { useSelector, useDispatch } from 'react-redux';
+
+
+export const appContacts = {
+  contacts: [],
+  filter: ""
+}
+
+export const store = configureStore({
+  reducer: {
+contacts: contactsSlice.reducer
+  },
+})
+
 const App = () => {
 
-  const [contacts, setContacts] = useState(
-    JSON.parse(localStorage.getItem('contacts')) ?? []
-  );
+  const dataContacts = useSelector(state => state)
+  console.log(dataContacts.contacts)
+  const dispatch = useDispatch();
+ 
+
+  // const [contacts, setContacts] = useState(
+  //   JSON.parse(localStorage.getItem('contacts')) ?? []
+  // );
   
   const [filter, setFilter] = useState('');
 
-  useEffect(() => {
-    window.localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts])
+  // useEffect(() => {
+  //   window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  // }, [contacts])
 
-  const addContact = (data) => {
-    if(duplicateName(data)) {
-      return toast.error(`${data.name} is already in contacts!`)
-    }
+  // const addContact = (data) => {
+  //   if(duplicateName(data)) {
+  //     return toast.error(`${data.name} is already in contacts!`)
+  //   }
 
-    const contact = {
-      id: nanoid(),
-      name: data.name,
-      number: data.number,
-    }
+  //   const contact = {
+  //     id: nanoid(),
+  //     name: data.name,
+  //     number: data.number,
+  //   }
    
-    setContacts(prevState => [contact, ...prevState]);
-  };
+  //   setContacts(prevState => [contact, ...prevState]);
+  // };
 
-  const removeContact = (id) => {
-    setContacts(prevState => prevState.filter((contact) => contact.id !== id));
-  };
+  // const removeContact = (id) => {
+  //   setContacts(prevState => prevState.filter((contact) => contact.id !== id));
+  // };
 
-  const filterNamesContacts = e => {
-    setFilter(e.currentTarget.value);
-  };
+  // const filterNamesContacts = e => {
+  //   setFilter(e.currentTarget.value);
+  // };
 
-  const getFilteredContacts = () => {
+  // const getFilteredContacts = () => {
  
-    if(!filter) {
-      return contacts;
-    }
+  //   if(!filter) {
+  //     return contacts;
+  //   }
 
-    const normalizedFilter = filter.toLocaleLowerCase();
+  //   const normalizedFilter = filter.toLocaleLowerCase();
 
-    const filteredContacts = contacts.filter(({ name }) => {
-      const normalizedName = name.toLocaleLowerCase();
-      return normalizedName.includes(normalizedFilter);
-    })
+  //   const filteredContacts = contacts.filter(({ name }) => {
+  //     const normalizedName = name.toLocaleLowerCase();
+  //     return normalizedName.includes(normalizedFilter);
+  //   })
 
-    return filteredContacts;
+  //   return filteredContacts;
     
-  };
+  // };
 
-  const duplicateName = ({ name }) => {
-    return contacts.find((contact) => contact.name === name);
-  };
+  // const duplicateName = ({ name }) => {
+  //   return contacts.find((contact) => contact.name === name);
+  // };
 
   return (
     <ContainerApp>
       <Title>Phonebook</Title>
       <ContactForm 
-        onSubmit={addContact}
+        onSubmit={()=> dispatch(addContact())}
       />
 
       <Subtitle>Contacts</Subtitle>
 
       <Filter 
         value={filter} 
-        onChange={filterNamesContacts}
+        // onChange={filterNamesContacts}
       />
 
       <ContactsList 
-        contacts={getFilteredContacts()} 
-        removeContact={removeContact}
+        // contacts={getFilteredContacts()} 
+        // removeContact={removeContact}
       />
 
       <ToastContainer 
