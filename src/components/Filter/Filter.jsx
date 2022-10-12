aea5ac44e39e8ec1f5d831bba686a175ce54b1ad
Darya <daryabobur@gmail.com>
2022-10-12 21:55:49 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,17 +1,52 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { Label } from "./FilterStyled";
+import { useDispatch, useSelector } from 'react-redux';
+import { filterName } from 'redux/slice';
 
-const Filter = ({ value, onChange }) => (
-    <Label>
+const Filter = () => {
+  const dispatch = useDispatch();
+  const dataFilter = useSelector(state => state.filter.filterContacts)
+
+  const filterNamesContacts = e => {
+    dispatch(filterName(e.target.value))
+  };
+
+
+
+  // const getFilteredContacts = () => {
+ 
+  //   if(!filter) {
+  //     return contacts;
+  //   }
+
+  //   const normalizedFilter = filter.toLocaleLowerCase();
+
+  //   const filteredContacts = contacts.filter(({ name }) => {
+  //     const normalizedName = name.toLocaleLowerCase();
+  //     return normalizedName.includes(normalizedFilter);
+  //   })
+
+  //   return filteredContacts;
+    
+  // };
+
+
+
+    return (
+      <Label>
       Find contacts by name
-      <input type="text" value={value} name="filter" onChange={onChange} />
-    </Label>
-  );
+      <input type="text" 
+      value={dataFilter} 
+      name="filter" 
+      onChange={filterNamesContacts} />
+    </Label>)
+    }
+  
 
 
-Filter.propTypes = {
-  value: PropTypes.string.isRequired,
-  onChange: PropTypes.func,
-}
+// Filter.propTypes = {
+
+//   onChange: PropTypes.func,
+// }
   
 export default Filter;
