2b028a6947d319dc532da8dbb3aabf4407ec96c2
Darya <daryabobur@gmail.com>
2022-10-12 21:55:55 +0300

    Update slice.js

--- src/redux/slice.js
+++ src/redux/slice.js
@@ -1,5 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { nanoid } from 'nanoid';
+import {  toast } from 'react-toastify';
+
+
+
 const contactsInitialState = [];
 
 export const contactsSlice = createSlice({
@@ -7,8 +11,9 @@ export const contactsSlice = createSlice({
     initialState: contactsInitialState,
     reducers: {
         addContact: {
+       
             reducer(state, action) {
-                state.push(action.payload);
+              state.push(action.payload);
               },
               prepare(text) {
                 return {
@@ -21,11 +26,40 @@ export const contactsSlice = createSlice({
               },
         },
         removeContact(state, action) {
-        const index = state.findIndex(task => task.id === action.payload);
+        const index = state.findIndex(contact => contact.id === action.payload);
         state.splice(index, 1);
-        }
+        },
+        duplicateName (state, action)  {
+          const duplicat = state.find((contact) => contact.name === action.payload);
+          // console.log(duplicat)
+          if(duplicat) {
+            toast.error(`${action.payload} is already in contacts!`)
+            return;
+          }
+          }
     },
   })
 
-  export const {addContact, removeContact} = contactsSlice.actions;
-  console.log(addContact)
+  export const {addContact, removeContact, duplicateName} = contactsSlice.actions;
+
+
+
+  const filtersInitialState = {
+    filterContacts: ''
+  };;
+
+  export const filtersSlice = createSlice({
+    name: "filter",
+    initialState: filtersInitialState,
+    reducers: {
+      filterName(state, action) {
+        state.filterContacts = action.payload;
+        console.log(action.payload)
+
+      },
+    },
+  });
+
+
+  export const {filterName} = filtersSlice.actions
+
