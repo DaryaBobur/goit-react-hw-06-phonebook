e343bd2cfc23c5507deb762f7812984fb21e0c0f
Darya <daryabobur@gmail.com>
2022-10-14 10:08:56 +0300

    Update ContactsList.jsx

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,32 +1,32 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid';
 import { Contacts } from './ContactsListStyled';
-import { useSelector, useDispatch } from 'react-redux';
-import {removeContact} from '../../redux/slice'
+import { useDispatch } from 'react-redux';
+import {removeContact} from '../../redux/contactsSlice'
 
-const ContactsList = () => {
+const ContactsList = ({ contacts }) => {
    const dispatch = useDispatch();
-   const contact = useSelector(state => state.contacts);
 
    return (  
       <Contacts>
-         {contact.map(({ name, number, id }) => 
+         {contacts.map(({ name, number, id }) => 
             <li key={nanoid()}>
                <p>- {name}: {number}</p>
                <button type='button' onClick={()=> dispatch(removeContact(id))}>Delete</button>
-            </li>)
+            </li>
+            )
          }
       </Contacts>
    )
 }
 
-ContactsList.propTypes = {
-   removeContact: PropTypes.func,
-   contacts: PropTypes.arrayOf(PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
-      id: PropTypes.string.isRequired,
-   }))
-}
+// ContactsList.propTypes = {
+//    removeContact: PropTypes.func,
+//    contacts: PropTypes.arrayOf(PropTypes.shape({
+//       name: PropTypes.string.isRequired,
+//       number: PropTypes.string.isRequired,
+//       id: PropTypes.string.isRequired,
+//    }))
+// }
 
 export default ContactsList;
