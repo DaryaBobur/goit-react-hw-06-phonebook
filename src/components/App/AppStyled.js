bacdad441fb1734da16cb818586a03c0cf5e7d9a
Darya <daryabobur@gmail.com>
2022-10-14 10:08:52 +0300

    Update AppStyled.js

--- src/components/App/AppStyled.js
+++ src/components/App/AppStyled.js
@@ -1,15 +1,15 @@
 import styled from 'styled-components';
 
 export const ContainerApp = styled.div`
-padding-top: 20px;
-padding-left: 30px;
+    padding-top: 20px;
+    padding-left: 30px;
 `
 
 export const Title = styled.h1`
-margin-bottom: 20px;
+    margin-bottom: 20px;
 `
 
 export const Subtitle = styled.h2`
-margin-bottom: 25px;
-font-size: 26px;
+    margin-bottom: 25px;
+    font-size: 26px;
 `
